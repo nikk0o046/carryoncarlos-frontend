83325673dc45fe3d21d364373f955643c8fd22a4
nikk0o046 <128469254+nikk0o046@users.noreply.github.com>
2023-08-02 22:27:09 +0300

    first real implementation of chat func

--- src/App.jsx
+++ src/App.jsx
@@ -1,65 +1,113 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './App.css' // Import the CSS file
+import ChatWindow from './components/ChatWindow'
 
 function App() {
-  const [user_request, setQuery] = useState('')
-  const [results, setResults] = useState([])
+  // First we initialize 'messages', 'isLoading' and 'errorMsg' states.
+  const [messages, setMessages] = useState([
+    {
+      role: "assistant",
+      content: "Hello there, fellow traveler! This is Carry-on Carlos speaking. I've weathered more baggage carousels and customs checkpoints than you've had hot dinners, so you can trust I'll find the right flights for you! To get us started, please tell me the city you're taking off from and give me a description of where you want to land."
+    }
+  ]);
   const [isLoading, setLoading] = useState(false)
   const [errorMsg, setErrorMsg] = useState(null)
 
-  const handleInputChange = (event) => {
-    setQuery(event.target.value)
+  // Console log every times "messages" changes
+  useEffect(() => {
+    console.log(`Updated conversation history: ${JSON.stringify(messages)}`);
+  }, [messages]);
+ 
+  // This function is used to handle function calls from the assistant
+  async function handleFunctionCall(functionCall) {
+    const { name, function_arguments } = functionCall; // The function call object will contain the name of the function and its arguments
+    const response = await fetch(`https://flight-buddy-service-ou44r5rafq-lz.a.run.app/search_flights`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(function_arguments)
+    });
+  
+    if (!response.ok) {
+      throw new Error(`Error ${response.status}: ${await response.text()}`);
+    } // If the response is not ok (status code is not 2xx), throw an error
+  
+    const data = await response.json(); // Parse the JSON response body
+    return data;
   }
-
-  const handleSearchClick = async () => {
-    setLoading(true)
+  
+  // This function is used to handle messages sent by the user
+  const handleSendMessage = async (newMessage) => {
+    console.log(`Received new message: ${JSON.stringify(newMessage)}`); // Log the new message
+    setLoading(true) // Add the new user message to the 'messages' state immediately
     setErrorMsg(null) // Clear any previous error messages
+    const updatedMessages = [...messages, newMessage]; // Create the updated messages array directly
+    setMessages(updatedMessages); // Update the state with the new messages array
+    console.log(`Sending the following conversation history to the server: ${JSON.stringify(updatedMessages)}`);
 
     try {
-      const response = await fetch('https://flight-buddy-service-ou44r5rafq-lz.a.run.app/search_flights', {
+      // We send a fetch request to the server with the conversation history.
+      const response = await fetch('http://localhost:8081/send_message', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
         },
-        body: JSON.stringify({user_request})
+        body: JSON.stringify({ conversationHistory: updatedMessages })
       })
 
+      // If the response is not ok (status code is not 2xx), handle the error
       if (!response.ok) {
         const errorData = await response.json();
         setErrorMsg(`Error ${response.status}: ${errorData.error}`); // Set the error message from the server response
-        setLoading(false);
+        setLoading(false); 
         return;
       }
 
-      const data = await response.json()
-      setResults(data)
+      const data = await response.json() // Parse the JSON response body
+      const newAssistantMessage = data.message; // Get the message field from the response
+      
+      if (newAssistantMessage.function_call) {
+      // If the assistant message includes a function call, send a predefined message to the user
+      const predefinedMessage = {
+        role: 'assistant',
+        content: "I've got all the info I need, wait a minute and I'll provide you the flights!",
+      };
+      setMessages(oldMessages => [...oldMessages, predefinedMessage])
+        // Then handle the function call
+
+        const functionResponse = await handleFunctionCall(newAssistantMessage.function_call);
+        //// We first check if the functionResponse array has any elements. If it's empty, it means no flights were found.
+        if (functionResponse.length > 0) {
+          // If we found flights, we transform each flight object into a readable string. We use map() to do this transformation for each flight in the functionResponse array.
+          const flights = functionResponse.map(flight => {
+            return `Flight number ${flight.flight_number} from ${flight.from} to ${flight.to} costs ${flight.price.value} ${flight.price.currency} with an average duration of ${flight.average_duration.hours} hours and ${flight.average_duration.minutes} minutes. You can book it [here](${flight.booking_link}).`
+          });
+          // We then combine all the flight strings into a single message that also includes an introduction. We use join() to concatenate all the flight strings, with each string on a new line.
+          newAssistantMessage.content = "Here are some options I found for you:\n" + flights.join("\n");
+        } else {
+          newAssistantMessage.content = "I'm sorry, but I couldn't find any flights that match your criteria."; 
+        }
+      }
+  
+      setMessages(oldMessages => [...oldMessages, newAssistantMessage]) // Add the new message from the assistant to the 'messages' state
     } catch (error) {
       setErrorMsg("Network error. Please try again later."); // Set a generic error message
     } finally {
-      setLoading(false);
+      setLoading(false); // After the request is completed (either successfully or with an error), set 'isLoading' to false
     }
   }
 
   return (
     <div className="app">
-      <h3>✈️ Just type what kind of trip you are looking for and when you'd want to go 🌍</h3>
-      <textarea className="search-input" value={user_request} onChange={handleInputChange} />
-      <button onClick={handleSearchClick}>Search</button>
-      {errorMsg && <div className="error">{errorMsg}</div>}
-      {isLoading && <p>Loading. This typically takes 15-30 seconds.</p>}
-      {results.map((result, index) => (
-        <div key={index}>
-          <h4>Flight Number: {result.flight_number}</h4>
-          <p>From: {result.from}</p>
-          <p>To: {result.to}</p>
-          <p>Price: {result.price.value} {result.price.currency}</p>
-          <p>Average Duration: {result.average_duration.hours}h {result.average_duration.minutes}m</p>
-          <a href={result.booking_link} target="_blank" rel="noopener">Book this flight</a>
-        </div>
-      ))}
+      <h1>🧳 Carry-on Carlos ✈️</h1>
+      <h3>Your personal flight search assistant</h3>
+      <ChatWindow sendMessageFunction={handleSendMessage} messages={messages} />
+      {errorMsg && <div className="error">{errorMsg}</div>} 
+      {isLoading && <p className="loading">Carry-on Carlos is searching for flights...</p>}
     </div>
-  )
+  );
+
 }
 
 export default App
-
---
+++ src/components/ChatWindow.jsx
@@ -0,0 +1,59 @@
+import React, { useState, useEffect, useRef } from 'react'
+import PropTypes from 'prop-types'
+
+// ChatWindow component receives sendMessageFunction (which sends user's messages to the server) 
+// and messages (the conversation history) as props from its parent (App)
+const ChatWindow = ({ sendMessageFunction, messages }) => {
+  const [message, setMessage] = useState('') // The message state variable is used to keep track of the current message being typed by the user
+  const messagesEndRef = useRef(null) // useRef is used to create a reference to the div that will be used to auto-scroll the chat window
+
+  const handleInputChange = (event) => { 
+    setMessage(event.target.value) // This function updates the 'message' state whenever the user types into the input field
+  }
+
+// This function is triggered when the user clicks on the send button
+  const handleSendClick = () => {
+    if (message.trim() !== '') {  // Prevent sending empty messages.
+        console.log(`Clicked "send" with message: ${message}`); // Log the user's message
+        sendMessageFunction({ role: 'user', content: message }) // We send a message object instead of just the text
+        setMessage('') // Clear text input field when message is sent
+      }
+  }
+
+  const scrollToBottom = () => {
+    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" })
+  }  // This function is used to auto-scroll the chat window to show the latest messages
+
+
+  // useEffect hook runs after every render. In this case, it's used to call scrollToBottom function 
+  // every time a new message is added to the 'messages' arra
+  useEffect(scrollToBottom, [messages])
+
+  return (
+    <div className="chat-window">
+      <div className="chat-messages">
+        {messages.map((message, index) => (
+          <div key={index} className={`chat-message ${message.role}`}>
+            <span>{message.content}</span>
+          </div>
+        ))}
+        <div ref={messagesEndRef} />
+      </div>
+      <div className="message-input-area">
+        <input type="text" value={message} onChange={handleInputChange} className="chat-input" />
+        <div className="send-button" onClick={handleSendClick} />
+      </div>
+    </div>
+  )
+}
+
+// Check that the correct type of props are being passed to ChatWindow
+ChatWindow.propTypes = {
+  sendMessageFunction: PropTypes.func.isRequired,
+  messages: PropTypes.arrayOf(PropTypes.shape({
+    role: PropTypes.string.isRequired,
+    content: PropTypes.string.isRequired,
+  })).isRequired,
+}
+
+export default ChatWindow
