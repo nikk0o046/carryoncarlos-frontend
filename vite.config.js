e4688804b91377618584ee229c3a8f8ce1143e0f
nikk0o046 <128469254+nikk0o046@users.noreply.github.com>
2023-08-28 20:47:40 +0300

    change homepage

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default ({ mode }) => defineConfig({
-  base: mode === 'production'
-    ? '/flight-frontend/'
-    : '/',
+export default defineConfig({
+  base: '/',
   plugins: [react()],
 })
-
